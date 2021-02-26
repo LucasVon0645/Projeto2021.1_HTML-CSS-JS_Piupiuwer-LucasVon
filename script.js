5d0ca4f482d314a1870f338121ece85179b4bf87
Lucas Von <lucas.vongarcia@gmail.com>
2021-02-26 13:55:05 -0300

    versao com responsividade, bug de pesquisa corrigido, filtragem e mensagem de erro melhorada

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-let postWrite = document.querySelector('#postWrite');
-let usersList;
+let postWrite = document.querySelector('.postWrite');
+let filterArray = [];
+let selectResultOnClick = false;
+
+
+
+const getPosts = async () => 
+{
+    try {
+
+        let response = await fetch("https://next.json-generator.com/api/json/get/EkyZfHLU_");
+        response = await response.json();
+
+        console.log(response);
+        addPostsfromOtherUsers(response);
+        
+        } catch {
+            console.log('Error')
+        }
+}
+
+
+
 
 function showNumberOfChars () 
 {
-    let number = this.value.length;
+    let postTextArea = document.querySelector('.postWrite');
+    let number = postTextArea.value.length;
     let error = document.querySelector('.postWriteErrorHidden');
-
-    document.querySelector('#cont').innerHTML = number+'/140';
+    let cont = document.querySelector('.cont');
+    cont.innerHTML = number+'/140';
 
     if (number > 140) {
+
         error.classList.add('postWriteErrorVisible');
+        cont.classList.add("warning");
+        postTextArea.classList.add("postWriteError")
         error.innerHTML = 'O limite máximo de <br>caracteres é 140'}
+
     else  {
+
+        postTextArea.classList.remove("postWriteError")
+        cont.classList.remove("warning");
         error.classList.remove('postWriteErrorVisible')}
 }
 
@@ -21,19 +50,24 @@ function addPostsfromOtherUsers (response)
 
         let post = document.createElement("div");
         post.classList.add("postOther");
+        post.classList.add("post");
         post.classList.add("postOtherLargeHeight");
-        addProfileInfo(post, postInformation);
+        post.classList.add("postOtherMobile");
+        post.classList.add("postOtherMobileSmall");
+
+        addProfileInfo(post, postInformation, false);
         addMessagePost(post, postInformation);
         addInteractionToPost(post);
 
-        let postArea = document.querySelector("#postArea");
+        let postArea = document.querySelector(".postArea");
         postArea.appendChild(post);
     });
 
 }
 
-function addMyPost () 
+function addMyPost (event) 
 {
+    event.preventDefault();
     let postInformation = getDataMyPost();
     console.log(postInformation);
     if (typeof postInformation === "number" ) {
@@ -42,29 +76,31 @@ function addMyPost ()
             error.classList.add('postWriteErrorVisible');
             error.innerHTML = 'Posts vazios não <br> são permitidos';
         }}
+
     else {
         let post = document.createElement("div");
         post.classList.add("myPost");
+        post.classList.add("post");
 
-        addProfileInfo(post, postInformation);
+        addProfileInfo(post, postInformation, true);
         addMessagePost(post, postInformation);
         addInteractionToMyPost(post);
 
-        let postArea = document.querySelector("#postArea");
+        let postArea = document.querySelector(".postArea");
         postArea.prepend(post);}
-    
-
 }
 
 
 function getDataMyPost () 
 {
-    let message = document.querySelector("#postWrite").value;
+    let message = document.querySelector(".postWrite").value;
     let postInformation = new Object ();
+
     if (message.length <= 140 && message.length != 0 ) {
         postInformation.mensagem = message;
         postInformation.nome = "Lucas Von";
         postInformation.username = "@LucasVon";
+        postInformation.imagem = 'imagens/Lucas.jpg'
         return postInformation;
     }
     
@@ -77,24 +113,25 @@ function currentDate ()
 {
 
     let today = new Date();
-    let day = String(today.getDay());
+    let day = String(today.getUTCDate());
     let month = String(today.getMonth() + 1);
     let year = String(today.getFullYear());
-    let hour = String(today.getUTCDate());
+    let hour = String(today.getHours());
     let minutes = String(today.getUTCMinutes());
-
+    console.log(today)
     return day + "/" + month + "/"  + year + " " + hour + "h" + minutes + "min";
 
 }
 
 
-function addProfileInfo (post, postInformation) 
+function addProfileInfo (post, postInformation, myPost) 
 {
 
     let imageProfile = document.createElement("img");
     if (postInformation.imagem) {
         imageProfile.src = postInformation.imagem;
-        imageProfile.classList.add("imagePost");}
+        if (myPost) imageProfile.classList.add("myImagePost");
+        else imageProfile.classList.add("imagePost");}
     else {
         imageProfile.src = "imagens/user_none.svg";
         imageProfile.classList.add("imagePostNone");}
@@ -107,6 +144,7 @@ function addProfileInfo (post, postInformation)
     
     let fullName = document.createElement("p");
     fullName.classList.add("fullName");
+    fullName.classList.add("fullNameMobile");
     fullName.innerHTML = postInformation.nome;
 
     let username = document.createElement("p");
@@ -161,7 +199,7 @@ function addLikesToPost (interaction, className)
 }
 
 
-function addHighlightToPost (interaction) 
+function addHighlightToPost (interaction, myPost) 
 {
 
     let highlight = document.createElement("img");
@@ -170,20 +208,21 @@ function addHighlightToPost (interaction)
 
     highlight.addEventListener("click", function () {
 
-    let post = this.parentNode.parentNode;
-    post.remove();
+        let post = this.parentNode.parentNode;
+        if (myPost) post = post.parentNode;
 
-    if (post.classList.contains('postHighlight')){
-        post.classList.remove("postHighlight");
-        this.src = "imagens/highlight.svg";
-    }
+        if (post.classList.contains('postHighlight')){
+            post.classList.remove("postHighlight");
+            this.src = "imagens/highlight.svg";
+        }
 
-    else {
-        post.classList.add("postHighlight");
-        this.src = "imagens/star_highlighted.svg";}
-    document.querySelector("#postArea").prepend(post);
+        else {
+            post.classList.add("postHighlight");
+            this.src = "imagens/star_highlighted.svg";
+            post.remove();
+            document.querySelector(".postArea").prepend(post);}
 
-    })
+        })
 
     interaction.appendChild(highlight);
 
@@ -197,7 +236,7 @@ function addInteractionToPost (post)
     interaction.classList.add("interaction");
 
     addLikesToPost(interaction, "likes");
-    addHighlightToPost(interaction);
+    addHighlightToPost(interaction,false);
 
     post.appendChild(interaction);
 }
@@ -210,11 +249,12 @@ function addInteractionToMyPost (post) {
     let divLikeHighlight = document.createElement("div");
     divLikeHighlight.classList.add("LikeHighlightmyPost");
     addLikesToPost(divLikeHighlight, "likesMyPost");
-    addHighlightToPost(divLikeHighlight);
+    addHighlightToPost(divLikeHighlight, true);
 
     let span = document.createElement("span");
     span.classList.add("saveChangeHidden")
     span.innerHTML = "Salvar";
+    span.addEventListener("click", savePostChange);
 
 
 
@@ -223,18 +263,20 @@ function addInteractionToMyPost (post) {
     edit.src = "imagens/editar.svg";
     edit.alt = "editar";
     edit.addEventListener("click", function () {
+        let postText = this.parentNode.parentNode.parentNode.childNodes[1];
         if (!this.classList.contains("editableModeIcon")){
             this.classList.add("editableModeIcon");
             this.src = "imagens/salvar.svg";
-            this.parentNode.parentNode.parentNode.childNodes[1].contentEditable = "true";
-            this.parentNode.parentNode.parentNode.childNodes[1].classList.add("editableBoxActive");
+            postText.contentEditable = "true";
+            postText.classList.add("editableBoxActive");
             this.parentNode.childNodes[1].classList.add("showUp");
         }
         else {
             this.classList.remove("editableModeIcon");
             this.src = "imagens/editar.svg";
-            this.parentNode.parentNode.parentNode.childNodes[1].contentEditable = "false";
-            this.parentNode.parentNode.parentNode.childNodes[1].classList.remove("editableBoxActive")}    
+            postText.contentEditable = "false";
+            postText.classList.remove("editableBoxActive")
+            this.parentNode.childNodes[1].classList.remove("showUp");}    
     })
 
 
@@ -264,59 +306,98 @@ function addInteractionToMyPost (post) {
 
 
 
-function cleanPostWrite () {
-    document.querySelector("#postWrite").value = "";
-    document.querySelector('#cont').innerHTML = '0'+'/140';
+function cleanPostWrite (event) {
+    event.preventDefault();
+    document.querySelector(".postWrite").value = "";
+    showNumberOfChars();
 }
 
-function getPosts () {
-
-    let xhr = new XMLHttpRequest();
-
-    xhr.open("GET", "https://next.json-generator.com/api/json/get/EkyZfHLU_");
-
-    xhr.addEventListener("load", function () {
-        let response = JSON.parse(xhr.responseText);
-        usersList = response;
-        console.log(response);
-        addPostsfromOtherUsers(response);
-    })
-
-    xhr.send();
-}
 
 getPosts();
+
+function openMenu() {
+    document.querySelector(".menu").style.width = "400px";
+  }
+
+function closeMenu() {
+    document.querySelector(".menu").style.width = "0";
+  }
+
+
+function bringResult () {
+    let result = this.innerHTML;
+    let searchResults = document.querySelector('.searchResultsDefault');
+    let searchBar = document.querySelector(".searchDefault")
+    searchBar.value = result;
+    searchUser();
+    searchResults.innerHTML = "";
+    searchBar.classList.remove("onSearch");
+
+}
 
 function searchUser () 
 {   
     let searchResults = document.querySelector('.searchResultsDefault');
-    let word = this.value;
+    let searchBar = document.querySelector(".searchDefault");
+    let word = searchBar.value;
     searchResults.innerHTML = "";
-    let searchBar = document.querySelector(".searchDefault")
+    let listOfPosts = document.querySelectorAll(".post")
     if (word.length > 0) {
+        document.querySelector(".searchIcon").classList.add("invisible");
         if (searchBar) searchBar.classList.add("onSearch");
         searchResults.classList.add("visible")
         let expression = new RegExp(word, "i");
-        usersList.forEach(element => {
-            if (expression.test(element.nome)) {
+        listOfPosts.forEach(element => {
+            nome = element.childNodes[0].childNodes[1].childNodes[0].innerHTML;
+            if (expression.test(nome)) {
                 let span = document.createElement('span');
-                span.innerHTML = element.nome;
+                span.innerHTML = nome;
+                span.addEventListener("click", bringResult);
                 searchResults.appendChild(span);
-            };
+                element.classList.remove("invisible");
+            }
+            else {
+                element.classList.add("invisible");
+            }
         });
-
+    
         if (Object.keys(searchResults.childNodes).length == 0) {
             let span = document.createElement('span');
             span.innerHTML = "Nenhum resultado encontrado";
-            searchResults.appendChild(span);}
+            searchResults.appendChild(span);}}
     
-    
+    else {
+        document.querySelector(".searchIcon").classList.remove("invisible");
+        searchBar.classList.remove("onSearch");
+        searchResults.classList.remove("visible");
+        listOfPosts.forEach( element => {
+            element.classList.remove("invisible");
+        })
+
     }
-    else searchBar.classList.remove("onSearch");
 }
 
+function savePostChange () 
+{
+    let postText = this.parentNode.parentNode.parentNode.childNodes[1];
+    postText.contentEditable = "false";
+    postText.classList.remove("editableBoxActive");
+    this.classList.remove("showUp");
+    this.parentNode.childNodes[0].src = "imagens/editar.svg";
+}
 
-document.querySelector("#postWrite").addEventListener("input", showNumberOfChars);
+$(window).resize(() => {
+    if (this.screen.width > 480) {
+        let menu = document.querySelector(".menu");
+        menu.style = "";
+    }
+})
+
+
+
+
+
+document.querySelector(".postWrite").addEventListener("input", showNumberOfChars);
 document.querySelector("#publishButton").addEventListener("click", addMyPost);
 document.querySelector("#cleanButton").addEventListener("click", cleanPostWrite);
 document.querySelector(".searchDefault").addEventListener("input", searchUser);
